2f8f563b9a64f490da173f562b41a420bea58588
Rishav Pramanik <rishavpramanik@gmail.com>
2022-02-05 02:18:09 +0530

    Update essemble_indi.py

--- essemble_indi.py
+++ essemble_indi.py
@@ -5,22 +5,19 @@ def essemble_indi(x_val, y_val,  model_name1, model_name2, model_name3, fold_no,
     test = x_val
 
     model_saved_name = model_name1 + "_" + str(fold_no) + ".h5"
-    model1 = tf.keras.models.load_model(
-        "/content/drive/MyDrive/SiPakMed/K_cross_val_models/" + model_saved_name)
+    model1 = tf.keras.models.load_model(MODEL1_PATH)
 
     # Generate generalization metrics
 
     preds1 = model1.predict(test, batch_size=validation_batch)
 
     model_saved_name = model_name2 + "_" + str(fold_no) + ".h5"
-    model2 = tf.keras.models.load_model(
-        "/content/drive/MyDrive/SiPakMed/K_cross_val_models/" + model_saved_name)
+    model2 = tf.keras.models.load_model(MODEL2_PATH)
 
     preds2 = model2.predict(test, batch_size=validation_batch)
 
     model_saved_name = model_name3 + "_" + str(fold_no) + ".h5"
-    model3 = tf.keras.models.load_model(
-        "/content/drive/MyDrive/SiPakMed/K_cross_val_models/" + model_saved_name)
+    model3 = tf.keras.models.load_model(MODEL3_PATH)
 
     preds3 = model3.predict(test, batch_size=validation_batch)
 
