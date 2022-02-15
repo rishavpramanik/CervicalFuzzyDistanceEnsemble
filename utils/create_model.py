153787fd2b432f6d3f6f3435c2489874c15dd37f
mb16biswas <64213233+mb16biswas@users.noreply.github.com>
2022-02-15 20:42:16 +0530

    Update create_model.py

--- utils/create_model.py
+++ utils/create_model.py
@@ -1,34 +1,32 @@
 import tensorflow as tf
 from tensorflow.keras.models import Model
 
-def create_model(model_name,IMG_SIZE = 256):
+def create_model(model_name,IMG_SIZE = 256, output = 5):
 
 
     IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)  # IMG_SIZE = 256
+    if(model_name == "MobileNetV2" ):
 
-    MobileNetV2_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
-                                                        include_top=False,
-                                                        weights='imagenet')
-
-    InceptionV3_model = tf.keras.applications.inception_v3.InceptionV3(input_shape=IMG_SHAPE,
-                                                                     include_top=False,
-                                                                     weights='imagenet')
-
-    InceptionResNetV2_model = tf.keras.applications.inception_resnet_v2.InceptionResNetV2(input_shape=IMG_SHAPE,
+        model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                                            include_top=False,
+                                                            weights='imagenet')
+    elif(model_name == "InceptionV3"):
+        model = tf.keras.applications.inception_v3.InceptionV3(input_shape=IMG_SHAPE,
+                                                                        include_top=False,
+                                                                        weights='imagenet')
+        
+    elif(model_name == "InceptionResNetV2"):
+        model = tf.keras.applications.inception_resnet_v2.InceptionResNetV2(input_shape=IMG_SHAPE,
                                                                                         include_top=False,
                                                                                         weights='imagenet')
-
-    models = {"MobileNetV2": MobileNetV2_model, "InceptionV3": InceptionV3_model,
-              "InceptionResNetV2": InceptionResNetV2_model}
-
-
-    model = models[model_name]
+    else:
+        return        
 
     x = tf.keras.layers.Conv2D(128, (3, 3), activation='relu')(model.output)
     x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
     x = tf.keras.layers.Flatten()(x)
     x = tf.keras.layers.Dense(100, activation='relu')(x)
-    x = tf.keras.layers.Dense(5, activation='softmax')(x)
+    x = tf.keras.layers.Dense(output, activation='softmax')(x)
 
     model = Model(inputs=model.input, outputs=x)
 
