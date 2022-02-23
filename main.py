309fb156392e7cc9b77ece4a1b248ed016425b7c
Rishav Pramanik <rishavpramanik@gmail.com>
2022-02-24 01:08:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--num_epochs', type=int, default = 70, help='Number of Epochs for training')
 parser.add_argument('--path', type=str, default = './', help='Path where the image data is stored')
+parser.add_argument('--batch_size', type=int, default = 16, help='Batch Size for Mini Batch Training')
+parser.add_argument('--lr', type=float, default = 1e-4, help='Learning rate for training')
 args = parser.parse_args()
 
 df = generate_csv(args.path)
@@ -25,4 +27,4 @@ k_fold_splits(x,y, files_for_train_x ,  files_for_validation_x , files_for_train
 n = len(files_for_train_x)
 for i in range(0,n):
 
-     k_fold_separate(files_for_train_x[i] , files_for_train_y[i] , files_for_validation_x[i] ,files_for_validation_y[i] , "InceptionV3" , "MobileNetV2" ,"InceptionResNetV2" ,i+1 , NUM_EPOCHS = args.num_epochs) 
+     k_fold_separate(files_for_train_x[i] , files_for_train_y[i] , files_for_validation_x[i] ,files_for_validation_y[i] , "InceptionV3" , "MobileNetV2" ,"InceptionResNetV2" ,i+1 , NUM_EPOCHS = args.num_epochs , train_batch=args.batch_size , validation_batch = args.batch_size, lr=args.lr) 
